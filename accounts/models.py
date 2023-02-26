e8f17af273ec417e93451bb0fa6641e3d996e6e4
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-02-26 14:39:09 -0500

    fix broken stuff

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    grade = models.IntegerField(choices=[(i, i) for i in range(9, 13)])
+    grade = models.IntegerField(choices=[(i, i) for i in range(9, 13)], default=9)
     points = models.IntegerField(default=0)
     events_interested = models.ManyToManyField('events.Event', blank=True)
