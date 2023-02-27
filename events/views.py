63f3c4805e8bd5ca7696fc5f8fc1ce3908a28512
AAZZAZRON <aaronzhu3.1415@gmail.com>
2023-02-26 22:48:37 -0500

    checklist admit

--- events/views.py
+++ events/views.py
@@ -28,3 +28,26 @@ def full_event(request, event_id):
             messages.success(request, "You are now interested in this event")
         return HttpResponseRedirect(request.path_info)
     return render(request, 'events/full_event.html', {'event': event})
+
+# admit users to event
+def admit(request, event_id):
+    try:
+        event = Event.objects.get(pk=event_id)
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist")
+    if not (request.user == event.organizer or request.user.is_staff or request.user.is_superuser):
+        raise Http404("You are not the organizer of this event")
+    
+    users = event.interested.all().order_by('-last_name')
+    if request.method == "POST":
+        checked_users = request.POST.getlist('admit')
+        for user in users:
+            if user.username in checked_users and user not in event.participants.all():
+                event.participants.add(user)
+                user.events_participated.add(event)
+            elif user.username not in checked_users and user in event.participants.all():
+                event.participants.remove(user)
+                user.events_participated.remove(event)
+        messages.success(request, "Attendance has been updated")
+        return HttpResponseRedirect(request.path_info)
+    return render(request, 'events/admit.html', {'event': event, 'users': users})
