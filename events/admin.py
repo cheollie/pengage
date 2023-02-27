e4fcc0b7277e4eda35b7a698415b73b7502444b0
Chelsea W <80985676+cheollie@users.noreply.github.com>
2023-02-26 22:53:31 -0500

    i forgor

--- events/admin.py
+++ events/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-from .models import Event, Tag, Prize
+from .models import Event, Tag
 
 admin.site.site_header = '[Website] Admin'
 admin.site.site_title = '[Website] Admin Areea'
 admin.site.index_text = 'Welcome to the [Website] Admin'
 
 admin.site.register(Event)
-admin.site.register(Prize)
 admin.site.register(Tag)
